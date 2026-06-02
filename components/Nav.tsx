e5e0a9cfd8972f2544132eb96a56971eb30d21ed
Officialkid <danielmwalili1@gmail.com>
2026-06-02 22:17:56 +0300

    feat: restore Pricing link in public navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -11,6 +11,7 @@ const navItems = [
   { title: "Features", href: "/#how-it-works", sectionId: "how-it-works" },
   { title: "How it works", href: "/how-it-works", sectionId: null },
   { title: "For Universities", href: "/for-universities", sectionId: null },
+  { title: "Pricing", href: "/pricing", sectionId: null },
   { title: "Get started", href: "/#get-started", sectionId: "get-started" },
 ]
 
